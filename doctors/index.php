9d4ff12fa2e211cc3d1707ac3f11d30daf40bc80
dayAlone <andrey.slider@gmail.com>
2014-04-20 02:39:17 +0600

    Doctors

---
+++ doctors/index.php
@@ -0,0 +1,36 @@
+<?
+	global $nav;
+	$nav='white';
+	require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+	$APPLICATION->SetTitle("Lilly Answers That Matter");
+?> 
+	<div id="products-promo">
+      <div class="container">
+         <h1>Сиалис® — эффективное и безопасное решение</h1>
+         <div class="row">
+            <div class="col-md-8">
+               <div id="video">
+                  <div class="icon"></div>
+               </div>
+            </div>
+            <div class="col-md-4">
+               <div id="product-1">
+
+                  <p>Сиалис® — одно решение двух проблем: восстанавливает эректильную функцию и улучшает мочеиспускание у мужчин с доброкачественной гиперплазией предстательной железы.<br><a href="#">ИНСТРУКЦИЯ</a></p>
+                  
+                  <a href="/doctors/5mg/"><img src="/layout/images/p-1.png" alt=""></a>
+               </div>
+               <div id="product-2">
+
+                  <p>Сиалис® позволяет мужчине улучшить качество сексуального здоровья и вернуться к жизни без эректильной дисфункции.<br><a href="#">ИНСТРУКЦИЯ</a></p>
+                  
+                  <a href="/doctors/20mg/"><img src="/layout/images/p-2.png" alt=""></a>
+               </div>
+            </div>
+         </div>
+         
+      </div>
+   </div>
+<?
+	require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");
+?>
