ca931e9da450c16c45aa88f622deca847586f621
dayAlone <andrey.slider@gmail.com>
2014-04-21 04:55:25 +0600

    CSS

--- bitrix/templates/ochenprosto/components/bitrix/news.detail/.default/template.php
+++ bitrix/templates/ochenprosto/components/bitrix/news.detail/.default/template.php
@@ -64,7 +64,58 @@
      <div class="content">
        <div class="container">
          <div id="chapter-list">
-           
+           <?
+              global $arFilter;
+              $arFilter = array('!ID'=>$arResult['ID']);
+              $APPLICATION->IncludeComponent(
+                "bitrix:news.list",
+                "more",
+                Array(
+                  "IBLOCK_TYPE" => "content",
+                  "IBLOCK_ID" => "4",
+                  "NEWS_COUNT" => "5",
+                  "SORT_BY1" => "SORT",
+                  "SORT_ORDER1" => "ASC",
+                  "SORT_BY2" => "",
+                  "SORT_ORDER2" => "",
+                  "FILTER_NAME" => "arFilter",
+                  "FIELD_CODE" => array("XML_ID","NAME","PREVIEW_TEXT","PREVIEW_PICTURE",""),
+                  "PROPERTY_CODE" => array("AUTHOR","HTML_TITLE"),
+                  "CHECK_DATES" => "Y",
+                  "DETAIL_URL" => "",
+                  "AJAX_MODE" => "N",
+                  "AJAX_OPTION_JUMP" => "N",
+                  "AJAX_OPTION_STYLE" => "N",
+                  "AJAX_OPTION_HISTORY" => "N",
+                  "CACHE_TYPE" => "A",
+                  "CACHE_TIME" => "36000000",
+                  "CACHE_FILTER" => "N",
+                  "CACHE_GROUPS" => "Y",
+                  "PREVIEW_TRUNCATE_LEN" => "",
+                  "ACTIVE_DATE_FORMAT" => "",
+                  "SET_STATUS_404" => "N",
+                  "SET_TITLE" => "N",
+                  "INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
+                  "ADD_SECTIONS_CHAIN" => "Y",
+                  "HIDE_LINK_WHEN_NO_DETAIL" => "N",
+                  "PARENT_SECTION" => $categorys[count($categorys)-1],
+                  "PARENT_SECTION_CODE" => "man",
+                  "INCLUDE_SUBSECTIONS" => "N",
+                  "DISPLAY_DATE" => "Y",
+                  "DISPLAY_NAME" => "Y",
+                  "DISPLAY_PICTURE" => "Y",
+                  "DISPLAY_PREVIEW_TEXT" => "Y",
+                  "PAGER_TEMPLATE" => "",
+                  "DISPLAY_TOP_PAGER" => "N",
+                  "DISPLAY_BOTTOM_PAGER" => "Y",
+                  "PAGER_TITLE" => "Новости",
+                  "PAGER_SHOW_ALWAYS" => "Y",
+                  "PAGER_DESC_NUMBERING" => "N",
+                  "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+                  "PAGER_SHOW_ALL" => "Y"
+                )
+              );
+              ?>
          </div>
          <div class="row">
            <div class="col-md-<?=($arResult['PROPERTIES']['FULLWIDTH']['VALUE_XML_ID']=='Y'?'12':'8')?>">
