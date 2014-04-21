0960a7996bc52df2538d4fe022f6c56c8c0a96e8
dayAlone <andrey.slider@gmail.com>
2014-04-22 04:28:15 +0600

    PDF

--- doctors/20mg/index.php
+++ doctors/20mg/index.php
@@ -12,7 +12,7 @@
                   Жизнь без эректильной дисфункции реальна. Сиалис® 20 мг. поможет вашему пациенту оперативно восстановить эректильную функцию и в течение 36 часов быть готовым к романтическим встречам и секуальным отношениям. Сиалис® 20мг. это уверенность в мужских силах на протяжении всего периода действия препарата.<br><br>
                   <a href="/doctors/" class="back"><span class="r">&lsaquo;</span> вернуться</a>
                </p>
-               <a href="/upload/cialis20.pdf" target="_blank" class="manual no-ajax">Инструкция</a>
+               <a href="/upload/сialis20.pdf" target="_blank" class="manual no-ajax">Инструкция</a>
                <a href="/doctors/5mg/" class="next"><span class="r">&lsaquo;</span> Сиалис® 5 мг.</a>
             </div>
          </div>
