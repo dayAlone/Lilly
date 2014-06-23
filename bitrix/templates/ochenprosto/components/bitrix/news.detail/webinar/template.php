eb8595e873e37481379a2ba13ea0c1f4508df3ce
dayAlone <andrey.slider@gmail.com>
2014-06-23 19:10:02 +0600

    Webinar

--- bitrix/templates/ochenprosto/components/bitrix/news.detail/webinar/template.php
+++ bitrix/templates/ochenprosto/components/bitrix/news.detail/webinar/template.php
@@ -119,7 +119,7 @@
          </div>
          <div class="row">
            <div class="col-md-<?=($arResult['PROPERTIES']['FULLWIDTH']['VALUE_XML_ID']=='Y'?'12':'8')?>">
-             <?=var_dump($arResult['PROPERTIES']['WEBINAR']['VALUE'])?>
+             <?=$arResult['PROPERTIES']['WEBINAR']['VALUE']['TEXT']?>
            </div>
           <? if(!$doctors) :?>
            <div class="col-md-4 <?=($arResult['PROPERTIES']['FULLWIDTH']['VALUE_XML_ID']=='Y'?'hidden':'')?> side" >
