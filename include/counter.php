5979574e0ba64c5c53441cc9d69b52d1c1cd4a07
dayAlone <andrey.slider@gmail.com>
2014-07-08 16:51:02 +0600

    Counter

--- include/counter.php
+++ include/counter.php
@@ -3,9 +3,14 @@
 ///////////////////////////////////////////////////////////////////////////////
  
 // api dependencies
-require_once $_SERVER['DOCUMENT_ROOT'].'/include/Google/Client.php';
-require_once $_SERVER['DOCUMENT_ROOT'].'/include/Google/Service/Analytics.php';
-
+if(file_exists($_SERVER['DOCUMENT_ROOT'].'/include/Google/Client.php')) {
+  require_once $_SERVER['DOCUMENT_ROOT'].'/include/Google/Client.php';
+  require_once $_SERVER['DOCUMENT_ROOT'].'/include/Google/Service/Analytics.php';
+}
+else {
+  require_once $_SERVER['DOCUMENT_ROOT'].'/include/google/Client.php';
+  require_once $_SERVER['DOCUMENT_ROOT'].'/include/google/Service/Analytics.php'; 
+}
 const CLIENT_ID         = '194409181325-kk7ludk25s8dfl8qnr0t3j2u184h8l0c.apps.googleusercontent.com';
 const SERVICE_EMAIL     = '194409181325-kk7ludk25s8dfl8qnr0t3j2u184h8l0c@developer.gserviceaccount.com';
 const KEY_FILE_PATH     = '/include/83b8b1c5fd6e902573366858ee24acf1ebb770a8-privatekey.p12';      
