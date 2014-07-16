0264b2616053c361ca8b24208fa796dc5aff4521
dayAlone <andrey.slider@gmail.com>
2014-07-16 21:54:57 +0600

    Init

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -1,4 +1,6 @@
 <?	
+	define("BX_COMPOSITE_DEBUG", true);
+	define("LOG_FILENAME", $_SERVER["DOCUMENT_ROOT"]."/../log.txt");
 	function mobile_title() {
 		global $APPLICATION;
 		if($APPLICATION->GetPageProperty('mobile_title')) {
