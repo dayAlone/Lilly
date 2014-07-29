28cdee1062f72e88b7afefb8db0cc45bff6e76ec
dayAlone <andrey.slider@gmail.com>
2014-07-30 01:29:13 +0600

    PDF

--- include/pdf.php
+++ include/pdf.php
@@ -1,5 +1,5 @@
 <?
-$html = file_get_contents("http://".$_SERVER[HTTP_HOST]."/test_man/print.php");
+$html = file_get_contents("http://".$_SERVER[HTTP_HOST]."/test_man/print.html");
 include("./mpdf/mpdf.php");
 
 $mpdf = new mPDF('utf-8', 'A4', '8', '', 10, 10, 7, 7, 10, 10); /*задаем формат, отступы и.т.д.*/
@@ -10,11 +10,11 @@ $mpdf->WriteHTML($stylesheet, 1);
 
 $mpdf->list_indent_first_level = 0; 
 
-$css = "http://".$_SERVER[HTTP_HOST]."/layout/css/bootstrap.min.css";
+$css = "http://".$_SERVER[HTTP_HOST]."/test_man/css/html5.css";
 $stylesheet = file_get_contents($css);
 $mpdf->WriteHTML($stylesheet,1);
 
-$css = "http://".$_SERVER[HTTP_HOST]."/layout/css/style.css";
+$css = "http://".$_SERVER[HTTP_HOST]."/test_man/css/style.css";
 $stylesheet = file_get_contents($css);
 $mpdf->WriteHTML($stylesheet,1);
 
