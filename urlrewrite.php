44f302f64f8b4b8e5206e88b435527d47f9b4ad2
dayAlone <andrey.slider@gmail.com>
2014-08-12 13:28:47 +0600

    URL

--- urlrewrite.php
+++ urlrewrite.php
@@ -7,14 +7,32 @@ $arUrlRewrite = array(
 		"PATH" => "/doctors/video/index.php",
 	),
 	array(
+		"CONDITION" => "#^/doctors/video/(.*)#",
+		"RULE" => "ELEMENT_CODE=\$1",
+		"ID" => "",
+		"PATH" => "/doctors/video/index.php",
+	),
+	array(
 		"CONDITION" => "#^/materials/(.*)/(.*)#",
 		"RULE" => "ELEMENT_CODE=\$1&\$2",
 		"ID" => "",
 		"PATH" => "/materials/index.php",
 	),
 	array(
+		"CONDITION" => "#^/materials/(.*)#",
+		"RULE" => "ELEMENT_CODE=\$1",
+		"ID" => "",
+		"PATH" => "/materials/index.php",
+	),
+	array(
 		"CONDITION" => "#^/woman/(.*)/(.*)#",
 		"RULE" => "ELEMENT_CODE=\$1&\$2",
+		"ID" => "",
+		"PATH" => "/woman/index.php",
+	),
+	array(
+		"CONDITION" => "#^/woman/(.*)#",
+		"RULE" => "ELEMENT_CODE=\$1",
 		"ID" => "",
 		"PATH" => "/woman/index.php",
 	),
