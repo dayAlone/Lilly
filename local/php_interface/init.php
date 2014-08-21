2e400e3c30e5d6dd5c0ad9e4c6acfaf27c38db21
dayAlone <andrey.slider@gmail.com>
2014-08-21 14:09:34 +0600

    Sections

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -1,6 +1,13 @@
 <?	
-	define("BX_COMPOSITE_DEBUG", true);
-	define("LOG_FILENAME", $_SERVER["DOCUMENT_ROOT"]."/log.txt");
+	AddEventHandler("iblock", "OnAfterIBlockSectionAdd", "OnAfterIBlockSectionAddHandler");
+	function OnAfterIBlockSectionAddHandler(&$arFields)
+    {
+        $SORT = $arFields["ID"]*100;
+        $props = array("SORT" => $SORT);
+        $bs = new CIBlockSection;
+        $bs->Update($arFields["ID"], $props);
+    }
+
 	function mobile_title() {
 		global $APPLICATION;
 		if($APPLICATION->GetPageProperty('mobile_title')) {
