f9129cc924ba46781dca1a5493094e2aae2b5e77
dayAlone <andrey.slider@gmail.com>
2014-12-03 16:59:33 +0500

    Changes

--- include/header.php
+++ include/header.php
@@ -19,7 +19,7 @@
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
-     
+    <meta name="robots" content="noarchive">
     <?php $APPLICATION->ShowHead();?>    
     
     
