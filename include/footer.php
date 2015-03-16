a81abb086997bc8f7e059d8a1b72429a231091e2
dayAlone <andrey.slider@gmail.com>
2015-03-16 20:25:20 +0500

    Footer

--- include/footer.php
+++ include/footer.php
@@ -175,6 +175,7 @@
   </script>
   <noscript><div><img src="//mc.yandex.ru/watch/21460912" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
   <!-- /Yandex.Metrika counter -->
+  <script type="text/javascript" async="async" src="http://track.hubrus.com/pixel?id=32150&type=js"></script>
 </body>
 <script type="text/javascript">(window.Image ? (new Image()) : document.createElement('img')).src = location.protocol + '//vk.com/rtrg?r=Bh6DI5ReSHIvsQsemv1SLKV6jKW8*qviv*I2ezvg/tqn4jZ439vgbfkzR2dG26Go/i4c*k6YC/B9MYPQWEEndGWVz/h81y8wBJjErUivrgccc5*4bzV4R7pAEENut9J/C8TyNd1rlJDD78OlhjcsIrNKOogMjYcoqSHHRNmNgbo-';</script>
 <? 
