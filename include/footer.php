050f4cae1d888bb8ec12e345086d6a4f85251c6f
dayAlone <andrey.slider@gmail.com>
2015-04-20 14:21:12 +0300

    Footer

--- include/footer.php
+++ include/footer.php
@@ -204,6 +204,16 @@ var s = d.getElementsByTagName('script')[0]; s.parentNode.insertBefore(pxl, s);
 } (document));
 </script>
 
+ <!-- Google Tag Manager -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-M6XD45"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-M6XD45');</script>
+<!-- End Google Tag Manager -->
+
 <? 
 
 $APPLICATION->SetAdditionalCSS("/layout/css/bootstrap.min.css", true);
